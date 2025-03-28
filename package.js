c1640095559e0912de75e58cbcb0965195ed9c70
e-tacosg <daniel.mysik@gmail.com>
2025-03-28 17:57:51 +0100

    Add files via upload

---
+++ package.js
@@ -0,0 +1,8 @@
+documedocument.addEventListener("DOMContentLoaded", function() {
+  let button = document.getElementById("profileButton");
+  if (button) {
+      button.addEventListener("click", function() {
+          window.location.href = "https://www.roblox.com/users/2488479952/profile";
+      });
+  }
+});
