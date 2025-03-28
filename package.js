60d400f976233b391f161ddb7a660a1fd62ca3fa
e-tacosg <daniel.mysik@gmail.com>
2025-03-28 18:39:10 +0100

    Delete package.js

--- package.js
+++
@@ -1,8 +0,0 @@
-documedocument.addEventListener("DOMContentLoaded", function() {
-  let button = document.getElementById("profileButton");
-  if (button) {
-      button.addEventListener("click", function() {
-          window.location.href = "https://www.roblox.com/users/2488479952/profile";
-      });
-  }
-});
